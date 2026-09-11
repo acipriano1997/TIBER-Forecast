70812b4fc2d13562c74d833e2b8e9b966e5a95f1
acipriano1997 <anthonyscipriano@gmail.com>
2026-09-11 08:08:29 -0400

    Add stable IRRIS model export surface

---
+++ src/models/irris/index.ts
@@ -0,0 +1,11 @@
+export { IRRIS_MODEL_VERSION, assessIrris } from './irrisEngine.js';
+export {
+  buildIrrisCalibrationBins,
+  reconcileIrrisAssessment,
+} from './irrisReconciliation.js';
+export type {
+  IrrisCalibrationBin,
+  IrrisObservedOutcome,
+  IrrisObservedScenario,
+  IrrisReconciliationResult,
+} from './irrisReconciliation.js';
